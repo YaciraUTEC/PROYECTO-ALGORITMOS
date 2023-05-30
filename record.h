25b0296b2788ce7a7d961edfb315f09286a8c2a8
César Eduardo Pajuelo Reyes <111761589+CesarPR30@users.noreply.github.com>
2023-05-29 21:36:36 -0500

    Record

---
+++ record.h
@@ -0,0 +1,13 @@
+#ifndef BLOCKCHIAN_RECORD_H
+#define BLOCKCHIAN_RECORD_H
+
+using namespace std;
+
+struct record {
+    string cliente;
+    string lugar;
+    double monto;
+    string fecha;
+};
+
+#endif //BLOCKCHIAN_RECORD_H
